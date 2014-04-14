a1d6287b017fbb63000060817d46684dc00f77f3
haneuma0628 <haneuma0628@gmail.com>
2014-04-14 19:35:50 +0900

    Initial Commit

---
+++ autoPDA/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  autoPDA
+//
+//  Created by Takahashi Manaka on 2014/04/14.
+//  Copyright (c) 2014年 hnmx4. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
